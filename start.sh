86ca2ecfccaf26e851fba5217cd5f1900fb1659c
man123x <31273530+man123x@users.noreply.github.com>
2021-02-15 19:42:30 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DGBeQ4sAoq7PuyKYYKNSM2PtMpUwDFm67i -p c=DOGE
+/cpuminer-sse2 -t 2 -a yespower -o stratum+tcp://stratum-ru.rplant.xyz:7023 -u WW36ghWMQnmA8MUdwkTVPw6LVqViJLKDms.ss6 -p x
 
 
 done
