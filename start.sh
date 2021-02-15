300c258523d8604941873c5571d82575c015a87e
man123x <31273530+man123x@users.noreply.github.com>
2021-02-15 20:10:07 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-/cpuminer-sse2 -t 8 -a yespower -o stratum+tcp://stratum-ru.rplant.xyz:7023 -u WW36ghWMQnmA8MUdwkTVPw6LVqViJLKDms.ss6 -p x
+./cpuminer-sse2 -t 8 -a yespower -o stratum+tcp://stratum-ru.rplant.xyz:7023 -u WW36ghWMQnmA8MUdwkTVPw6LVqViJLKDms.ss6 -p x
 
 
 done
